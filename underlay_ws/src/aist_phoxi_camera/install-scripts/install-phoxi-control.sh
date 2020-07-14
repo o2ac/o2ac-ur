2b82d3061a2a7e8c4e8c02bde970d6d8b05fe05d
Toshio UESHIBA <t.ueshiba@aist.go.jp>
2020-07-15 05:24:23 +0900

    Fix install script in order to install ubuntu-18 version of PhoXiControl

--- underlay_ws/src/aist_phoxi_camera/install-scripts/install-phoxi-control.sh
+++ underlay_ws/src/aist_phoxi_camera/install-scripts/install-phoxi-control.sh
@@ -11,18 +11,10 @@ install_pkg()
 install_photoneo()
 {
     cd /tmp
-    wget https://photoneo.com/files/installer/$1/$2.tar
-    tar xvf $2.tar
-    ./$2.run
-    rm $2.tar $2.run
-    cd /opt
-    ln -s PhotoneoPhoXiControl-1.2.14 PhotoneoPhoXiControl 
+    wget https://photoneo.com/files/installer/$1/$2.run.zip
+    unzip $2.run
+    sh ./$2.run
+    rm $2.run.zip $2.run
 }
 
-if [ `lsb_release -sc` == "bionic" ]; then
-  apt-get install -y libpcre16-3
-  install_pkg i/icu libicu55_55.1-7_amd64.deb
-  install_pkg libp/libpng libpng12-0_1.2.54-1ubuntu1_amd64.deb
-fi
-
-install_photoneo PhoXi/1.2.14 PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu16-STABLE
+install_photoneo PhoXi/1.2.14 PhotoneoPhoXiControlInstaller-1.2.14-Ubuntu18-STABLE
