49827b3db17b059818514566664bd6a019d79d97
Felix von Drigalski <FvDrigalski@gmail.com>
2020-09-09 15:10:24 +0900

    Finish O2AC Rviz panel (background processing, vacuum buttons)

--- catkin_ws/src/o2ac_rviz/src/o2ac_rviz.cpp
+++ catkin_ws/src/o2ac_rviz/src/o2ac_rviz.cpp
@@ -55,12 +55,35 @@ private slots:
     void on_button_loosen_nut_1s_clicked();
     void on_button_loosen_m2s_1s_clicked();
 
+    void on_button_vacuum_m3_clicked();
+    void on_button_vacuum_m4_clicked();
+
 // methods
 private:
     void set_button_active(QPushButton *button, const bool active);
+    
     void io_state_callback(const ur_msgs::IOStates &states);
-
     void update_status(const ros::TimerEvent&);
+
+    // The below 2 functions are taken from "planning_scene_display.h" *(MoveIt)
+    
+    // TODO: Add queue for background jobs (or just live with the buttons not being perfect)
+    // /** Queue this function call for execution within the background thread
+    // All jobs are queued and processed in order by a single background thread. */
+    // void addBackgroundJob(const boost::function<void()>& job, const std::string& name);
+
+    /** Directly spawn a (detached) background thread for execution of this function call
+         Should be used, when order of processing is not relevant / job can run in parallel.
+        Must be used, when job will be blocking. Using addBackgroundJob() in this case will block other queued jobs as
+        well */
+    void spawnBackgroundJob(const boost::function<void()>& job);
+
+    void robots_go_home();
+
+    void open_gripper_wrapper(std::string robot);
+    void close_gripper_wrapper(std::string robot);
+    void go_named_pose_wrapper(std::string robot_name, std::string pose);
+    void equip_unequip_wrapper(std::string robot_name, std::string tool, std::string equip_or_unequip);
 
 // members
 private:
@@ -72,12 +95,19 @@ private:
     ros::Subscriber sub_io_states;
     ros::Timer update_status_timer_;
 
-    actionlib::SimpleActionClient<o2ac_msgs::changeToolAction> changeToolActionClient_; // Never used, just checked
+    actionlib::SimpleActionClient<o2ac_msgs::changeToolAction> changeToolActionClient_; // Only used for heartbeat
 
-    QString red_style = "QLabel { background-color : red; color : black; }";
-    QString yellow_style = "QLabel { background-color : yellow; color : yellow; }";
-    QString green_style = "QLabel { background-color : green; color : green; }";
-    QString grey_style = "QLabel { background-color : grey; color : black; }";
+    QString red_label_style = "QLabel { background-color : red; color : black; }";
+    QString yellow_label_style = "QLabel { background-color : yellow; color : yellow; }";
+    QString green_label_style = "QLabel { background-color : green; color : green; }";
+    QString grey_label_style = "QLabel { background-color : grey; color : black; }";
+
+    QString red_button_style = "QPushButton { background-color : red; color : black; }";
+    QString yellow_button_style = "QPushButton { background-color : yellow; color : black; }";
+    QString green_button_style = "QPushButton { background-color : green; color : green; }";
+    QString grey_button_style = "QPushButton { background-color : grey; color : black; }";
+
+    bool m3_suction_on, m4_suction_on;
 };
 
 O2ACSetupPanel::O2ACSetupPanel(QWidget* parent) : rviz::Panel(parent), changeToolActionClient_("/o2ac_skills/change_tool", true)
@@ -90,36 +120,24 @@ O2ACSetupPanel::O2ACSetupPanel(QWidget* parent) : rviz::Panel(parent), changeToo
     ui.setupUi(this);
 
     // Default status is RED
-    ui.label_tool_m3_vac->setStyleSheet(grey_style);
-    ui.label_tool_m4_vac->setStyleSheet(grey_style);
-    ui.label_robots_online->setStyleSheet(red_style);
-    ui.label_skill_server_online->setStyleSheet(red_style);
-    ui.label_tools_online->setStyleSheet(red_style);
-    ui.label_tools_online_2->setStyleSheet(red_style);
+    ui.label_robots_online->setStyleSheet(red_label_style);
+    ui.label_skill_server_online->setStyleSheet(red_label_style);
+    ui.label_tools_online->setStyleSheet(red_label_style);
+    ui.label_tools_online_2->setStyleSheet(red_label_style);
 
     update_status_timer_ = n.createTimer(ros::Duration(0.5), &O2ACSetupPanel::update_status, this);
 
-    // Disabling buttons that don't work yet. 
-    // TODO: Figure out why those commands freeze (move() does not return for some reason)
-    ui.button_robots_home->setDisabled(true);
-    ui.button_home_a_bot->setDisabled(true);
-    ui.button_home_b_bot->setDisabled(true);
-
-    ui.button_a_bot_m3_equip->setDisabled(true);
-    ui.button_a_bot_m3_unequip->setDisabled(true);
-    ui.button_a_bot_m3_pick_screw->setDisabled(true);
-
-    ui.button_b_bot_m4_equip->setDisabled(true);
-    ui.button_b_bot_m4_unequip->setDisabled(true);
-    ui.button_b_bot_m4_pick_screw->setDisabled(true);
-
-    ui.button_a_bot_nut_equip->setDisabled(true);
-    ui.button_a_bot_nut_unequip->setDisabled(true);
-
-    ui.button_b_bot_m2s_equip->setDisabled(true);
-    ui.button_b_bot_m2s_unequip->setDisabled(true);
-
     //TODO: Add buttons for vacuum
+}
+
+
+// void O2ACSetupPanel::addBackgroundJob(const boost::function<void()>& job, const std::string& name)
+// {
+    
+// }
+void O2ACSetupPanel::spawnBackgroundJob(const boost::function<void()>& job)
+{
+  boost::thread t(job);
 }
 
 void O2ACSetupPanel::update_status(const ros::TimerEvent& event)
@@ -127,48 +145,75 @@ void O2ACSetupPanel::update_status(const ros::TimerEvent& event)
     // Check tools
     if (ss_.fastening_tool_client.waitForServer(ros::Duration(0.2)))
     {
-        ui.label_tools_online->setStyleSheet(green_style);
-        ui.label_tools_online_2->setStyleSheet(green_style);
+        ui.label_tools_online->setStyleSheet(green_label_style);
+        ui.label_tools_online_2->setStyleSheet(green_label_style);
     }
     else
     {
-        ui.label_tools_online->setStyleSheet(red_style);
-        ui.label_tools_online_2->setStyleSheet(red_style);
+        ui.label_tools_online->setStyleSheet(red_label_style);
+        ui.label_tools_online_2->setStyleSheet(red_label_style);
     }
 
     // Check skill server
     if (changeToolActionClient_.waitForServer(ros::Duration(0.2)))
-        ui.label_skill_server_online->setStyleSheet(green_style);
+        ui.label_skill_server_online->setStyleSheet(green_label_style);
     else
-        ui.label_skill_server_online->setStyleSheet(red_style);
+        ui.label_skill_server_online->setStyleSheet(red_label_style);
     
     if (ss_.a_bot_ros_control_active_ && ss_.b_bot_ros_control_active_)
-        ui.label_robots_online->setStyleSheet(green_style);
+        ui.label_robots_online->setStyleSheet(green_label_style);
     else
-        ui.label_robots_online->setStyleSheet(red_style);
+        ui.label_robots_online->setStyleSheet(red_label_style);
 }
 
 void O2ACSetupPanel::io_state_callback(const ur_msgs::IOStates &states) {
-    if (states.digital_in_states[0].state) // suction m3
+    // First check output (suction on/off)
+    if (states.digital_out_states[0].state) // suction m3
     {
-        ui.label_tool_m3_vac->setText("O");
-        ui.label_tool_m3_vac->setStyleSheet(green_style);
+        m3_suction_on = true;
+        ui.button_vacuum_m3->setText("O");
+    }
+    else
+    {
+        m3_suction_on = false;
+        ui.button_vacuum_m3->setText("-");
+    }
+        
+    if (states.digital_out_states[1].state) // suction m4
+    {
+        m4_suction_on = true;
+        ui.button_vacuum_m4->setText("O");
+    }
+    else
+    {
+        m4_suction_on = false;
+        ui.button_vacuum_m4->setText("-");
+    }
+
+    // Then check vacuum (suction seal)
+    if (states.digital_in_states[0].state) // vacuum m3
+    {
+        ui.button_vacuum_m3->setStyleSheet(green_button_style);
     }
     else 
     {
-        ui.label_tool_m3_vac->setText("");
-        ui.label_tool_m3_vac->setStyleSheet(grey_style);
+        if (m3_suction_on)
+            ui.button_vacuum_m3->setStyleSheet(yellow_button_style);
+        else
+            ui.button_vacuum_m3->setStyleSheet(grey_button_style);
+        
     }
     
-    if (states.digital_in_states[1].state) // suction m4
+    if (states.digital_in_states[1].state) // vacuum m4
     {
-        ui.label_tool_m4_vac->setText("O");
-        ui.label_tool_m4_vac->setStyleSheet(green_style);
+        ui.button_vacuum_m4->setStyleSheet(green_button_style);
     }
     else 
     {
-        ui.label_tool_m4_vac->setText("");
-        ui.label_tool_m4_vac->setStyleSheet(grey_style);
+        if (m4_suction_on)
+            ui.button_vacuum_m4->setStyleSheet(yellow_button_style);
+        else
+            ui.button_vacuum_m4->setStyleSheet(grey_button_style);
     }
 }
 
@@ -180,35 +225,38 @@ void O2ACSetupPanel::on_button_activate_ros_control_clicked()
 
 void O2ACSetupPanel::on_button_robots_home_clicked()
 {
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::robots_go_home, this));  // Required for background processing
+}
+void O2ACSetupPanel::robots_go_home()
+{
     ss_.goToNamedPose("home", "a_bot", 0.2, 0.2);
     ss_.goToNamedPose("home", "b_bot", 0.2, 0.2);
 }
 
-
 void O2ACSetupPanel::on_button_home_a_bot_clicked()
 {
-    ss_.goToNamedPose("home", "a_bot", 0.5, 0.5);
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::go_named_pose_wrapper, this, "a_bot", "home"));  // Required for background processing
 }
 void O2ACSetupPanel::on_button_open_gripper_a_clicked()
 {
-    ss_.openGripper("a_bot");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::open_gripper_wrapper, this, "a_bot"));
 }
 void O2ACSetupPanel::on_button_close_gripper_a_clicked()
 {
-    ss_.closeGripper("a_bot");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::close_gripper_wrapper, this, "a_bot"));
 }
 
 void O2ACSetupPanel::on_button_home_b_bot_clicked()
 {
-    ss_.goToNamedPose("home", "b_bot", 0.5, 0.5);
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::go_named_pose_wrapper, this, "b_bot", "home"));  // Required for background processing
 }
 void O2ACSetupPanel::on_button_open_gripper_b_clicked()
 {
-    ss_.openGripper("b_bot");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::open_gripper_wrapper, this, "b_bot"));
 }
 void O2ACSetupPanel::on_button_close_gripper_b_clicked()
 {
-    ss_.closeGripper("b_bot");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::close_gripper_wrapper, this, "b_bot"));
 }
 
 void O2ACSetupPanel::on_button_open_base_fixation_clicked()
@@ -226,46 +274,46 @@ void O2ACSetupPanel::on_button_close_base_fixation_clicked()
 
 void O2ACSetupPanel::on_button_a_bot_m3_equip_clicked()
 {
-    ss_.equipUnequipScrewTool("a_bot", "screw_tool_m3", "equip");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::equip_unequip_wrapper, this, "a_bot", "screw_tool_m3", "equip"));
 }
 void O2ACSetupPanel::on_button_a_bot_m3_unequip_clicked()
 {
-    ss_.equipUnequipScrewTool("a_bot", "screw_tool_m3", "unequip");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::equip_unequip_wrapper, this, "a_bot", "screw_tool_m3", "unequip"));
 }
 void O2ACSetupPanel::on_button_a_bot_m3_pick_screw_clicked()
 {
-    ss_.equipUnequipScrewTool("a_bot", "screw_tool_m3", "equip");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::equip_unequip_wrapper, this, "a_bot", "screw_tool_m3", "equip"));
 }
 
 void O2ACSetupPanel::on_button_b_bot_m4_equip_clicked()
 {
-    ss_.equipUnequipScrewTool("b_bot", "screw_tool_m4", "equip");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::equip_unequip_wrapper, this, "b_bot", "screw_tool_m4", "equip"));
 }
 void O2ACSetupPanel::on_button_b_bot_m4_unequip_clicked()
 {
-    ss_.equipUnequipScrewTool("b_bot", "screw_tool_m4", "unequip");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::equip_unequip_wrapper, this, "b_bot", "screw_tool_m4", "unequip"));
 }
 void O2ACSetupPanel::on_button_b_bot_m4_pick_screw_clicked()
 {
-    ss_.equipUnequipScrewTool("b_bot", "screw_tool_m4", "equip");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::equip_unequip_wrapper, this, "b_bot", "screw_tool_m4", "equip"));
 }
 
 void O2ACSetupPanel::on_button_a_bot_nut_equip_clicked()
 {
-    ss_.equipUnequipScrewTool("a_bot", "nut_tool_m6", "equip");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::equip_unequip_wrapper, this, "a_bot", "nut_tool_m6", "equip"));
 }
 void O2ACSetupPanel::on_button_a_bot_nut_unequip_clicked()
 {
-    ss_.equipUnequipScrewTool("a_bot", "nut_tool_m6", "unequip");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::equip_unequip_wrapper, this, "a_bot", "nut_tool_m6", "unequip"));
 }
 
 void O2ACSetupPanel::on_button_b_bot_m2s_equip_clicked()
 {
-    ss_.equipUnequipScrewTool("b_bot", "set_screw_tool", "equip");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::equip_unequip_wrapper, this, "b_bot", "set_screw_tool", "equip"));
 }
 void O2ACSetupPanel::on_button_b_bot_m2s_unequip_clicked()
 {
-    ss_.equipUnequipScrewTool("b_bot", "set_screw_tool", "unequip");
+    spawnBackgroundJob(boost::bind(&O2ACSetupPanel::equip_unequip_wrapper, this, "b_bot", "set_screw_tool", "unequip"));
 }
 
 
@@ -311,6 +359,33 @@ void O2ACSetupPanel::on_button_loosen_m2s_1s_clicked()
     ss_.sendFasteningToolCommand("set_screw_tool", "loosen", false, 1.0);
 }
 
+void O2ACSetupPanel::on_button_vacuum_m3_clicked()
+{
+    ss_.setSuctionEjection("screw_tool_m3", !m3_suction_on);
+}
+void O2ACSetupPanel::on_button_vacuum_m4_clicked()
+{
+    ss_.setSuctionEjection("screw_tool_m4", !m4_suction_on);
+}
+
+
+// Wrappers to allow background processing
+void O2ACSetupPanel::open_gripper_wrapper(std::string robot)
+{
+    ss_.openGripper(robot);
+}
+void O2ACSetupPanel::close_gripper_wrapper(std::string robot)
+{
+    ss_.closeGripper(robot);
+}
+void O2ACSetupPanel::go_named_pose_wrapper(std::string robot_name, std::string pose)
+{
+    ss_.goToNamedPose(pose, robot_name, 0.2, 0.2);
+}
+void O2ACSetupPanel::equip_unequip_wrapper(std::string robot_name, std::string tool, std::string equip_or_unequip)
+{
+    ss_.equipUnequipScrewTool(robot_name, tool, equip_or_unequip);
+}
 
 // --- 
 
