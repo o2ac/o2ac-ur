6ce09711119706c25bd374fc962686e0d6dc3722
Shuichi Akizuki <s-akizuki@sist.chukyo-u.ac.jp>
2020-12-25 11:19:55 +0000

    recognition for backside

--- catkin_ws/src/o2ac_vision/scripts/o2ac_ssd.py
+++ catkin_ws/src/o2ac_vision/scripts/o2ac_ssd.py
@@ -1,5 +1,5 @@
 #-*- encoding:utf-8 -*-
-import rospkg
+#import rospkg
 import os
 import sys
 import glob, json, time
@@ -12,43 +12,50 @@ import numpy as np
 import cv2
 if torch.cuda.is_available():
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
-rospack = rospkg.RosPack()
-sys.path.append(rospack.get_path("wrs_dataset") + '/ssd.pytorch')
+#rospack = rospkg.RosPack()
+#sys.path.append(rospack.get_path("wrs_dataset") + '/ssd.pytorch')
 
 from ssd import build_ssd
 from data import WRS2020_Detection, WRS_ROOT, AnnotationTransform
 from data import WRS2020_CLASSES as labels
 
 # ID Mapping.
-# Chukyo label -> O2AC label
+# Chukyo label -> O2AC label(ID, Name, State{front:0, back:1})
 o2ac_label = {
-    0:(1,"01-BASE"),
-    1:(3,"03-PLATE2"),
-    2:(2,"02-PLATE"),
-    3:(4,"04_37D-GEARMOTOR-50-70"),
-    4:(11,"11_MBRAC60-2-10"),
-    5:(7,"07_SBARB6200ZZ_30"),
-    6:(13,"13_MBGA30-2"),
-    7:(13,"13_MBGA30-2"),
-    8:(5,"05_MBRFA30-2-P6"),
-    9:(14,"14_BGPSL6-9-L30-F7"),
-    10:(8,"08_SSFHRT10-75-M4-FC55-G20"),
-    11:(6,"06_MBT4-400"),
-    12:(9,"09_EDCS10"),
-    13:(9,"09_EDCS10"),
-    14:(12,"12_CLBUS6-9-9.5"),
-    15:(10,"10_CLBPS10_17_4")
+    0:(1,"01-BASE",0),
+    1:(3,"03-PLATE2",0),
+    2:(2,"02-PLATE",0),
+    3:(4,"04_37D-GEARMOTOR-50-70",0),
+    4:(11,"11_MBRAC60-2-10",0),
+    5:(7,"07_SBARB6200ZZ_30",0),
+    6:(13,"13_MBGA30-2",0),
+    7:(13,"13_b_MBGA30-2",1),
+    8:(5,"05_MBRFA30-2-P6",0),
+    9:(14,"14_BGPSL6-9-L30-F7",0),
+    10:(8,"08_SSFHRT10-75-M4-FC55-G20",0),
+    11:(6,"06_MBT4-400",0),
+    12:(9,"09_EDCS10",0),
+    13:(9,"09_b_EDCS10",1),
+    14:(12,"12_CLBUS6-9-9.5",0),
+    15:(10,"10_CLBPS10_17_4",0),
+    16:(3,"03_b_PLATE2",1),
+    17:(2,"02_b_PLATE",1),
+    18:(11,"11_b_MBRAC60-2-10",1),
+    19:(7,"07_b_SBARB6200ZZ_30",1),
+    20:(12,"12_b_CLBUS6-9-9.5",1)
 }
 
-annotation_root = rospack.get_path("wrs_dataset") + "/Annotations/Far/Image-wise/*.json"
+
+#annotation_root = rospack.get_path("wrs_dataset") + "/Annotations/Far/Image-wise/*.json"
+annotation_root = "wrs_dataset/Annotations/Far/Image-wise/*.json"
 # アノテーションファイルの取得
 annotations = glob.glob(annotation_root)
 
 class ssd_detection():
 
     def __init__(self):
-        fname_weight = rospack.get_path("wrs_dataset") + "/ssd.pytorch/WRS_lr1e3_bs16.pth"
-        self.net = build_ssd('test', 300, 17)    # initialize SSD
+        fname_weight = rospack.get_path("wrs_dataset") + "/ssd.pytorch/WRS.pth"
+        self.net = build_ssd('test', 300, 22)    # initialize SSD
         self.net.load_weights( fname_weight )
 
     def object_detection(self, im_in, im_vis=None, threshold = 0.6):
@@ -89,7 +96,11 @@ class ssd_detection():
                 j+=1
 
                 bbox = [ int(coords[0][0]), int(coords[0][1]), int(coords[1]), int(coords[2])]
-                result = {"bbox": bbox, "class": i-1, "confidence": score}
+                result = {"bbox": bbox, 
+                          "class": o2ac_label[i-1][0], 
+                          "confidence": score, 
+                          "name":o2ac_label[i-1][1],
+                          "state":o2ac_label[i-1][2]}
                 results.append( result )
 
         if im_vis is None:
@@ -97,16 +108,18 @@ class ssd_detection():
 
         for res in results:
             bbox = res["bbox"]
+            bb_color = (0,255,0)
+            if res["state"] == 1:
+                bb_color = (0,0,255)
             im_vis = cv2.rectangle( im_vis, (bbox[0],  bbox[1]),
                                     (bbox[0]+bbox[2], bbox[1]+bbox[3]),
-                                    (0,255,0), 3 )
-            cv2.putText( im_vis, o2ac_label[res["class"]][1],
+                                    bb_color, 3 )
+            cv2.putText( im_vis, res["name"],
                          (bbox[0], bbox[1]),1, 0.7, (255,255,255), 2, cv2.LINE_AA )
-            cv2.putText( im_vis, o2ac_label[res["class"]][1],
+            cv2.putText( im_vis, res["name"],
                          (bbox[0], bbox[1]),1, 0.7, (255,0,0), 1, cv2.LINE_AA )
 
-        for j in range(len(results)):
-            results[j]["class"] = o2ac_label[results[j]["class"]][0]
+
 
         # cv2.imwrite("ssd_result.png", im_vis)
 
