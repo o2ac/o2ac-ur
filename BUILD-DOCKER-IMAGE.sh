9a0c370dc5b76b2f14eee8e2ad892271e93c361d
Felix von Drigalski <FvDrigalski@gmail.com>
2021-02-11 12:03:46 +0900

    Renaming

--- BUILD-DOCKER-IMAGE.sh
+++ BUILD-DOCKER-IMAGE.sh
@@ -4,24 +4,24 @@
 
 # Set the Docker container name from a project name (first argument).
 # If no argument is given, use the current user name as the project name.
-PROJECT=$1
-if [ -z "${PROJECT}" ]; then
-  PROJECT=${USER}
+DOCKER_PROJECT=$1
+if [ -z "${DOCKER_PROJECT}" ]; then
+  DOCKER_PROJECT=${USER}
 fi
-CONTAINER="${PROJECT}_o2ac-ur_1"
-echo "$0: PROJECT=${PROJECT}"
-echo "$0: CONTAINER=${CONTAINER}"
+DOCKER_CONTAINER="${DOCKER_PROJECT}_o2ac-ur_1"
+echo "$0: DOCKER_PROJECT=${DOCKER_PROJECT}"
+echo "$0: DOCKER_CONTAINER=${DOCKER_CONTAINER}"
 
 # Stop and remove the Docker container.
-EXISTING_CONTAINER_ID=`docker ps -aq -f name=${CONTAINER}`
-if [ ! -z "${EXISTING_CONTAINER_ID}" ]; then
-  echo "Stop the container ${CONTAINER} with ID: ${EXISTING_CONTAINER_ID}."
-  docker stop ${EXISTING_CONTAINER_ID}
-  echo "Remove the container ${CONTAINER} with ID: ${EXISTING_CONTAINER_ID}."
-  docker rm ${EXISTING_CONTAINER_ID}
+EXISTING_DOCKER_CONTAINER_ID=`docker ps -aq -f name=${DOCKER_CONTAINER}`
+if [ ! -z "${EXISTING_DOCKER_CONTAINER_ID}" ]; then
+  echo "Stop the container ${DOCKER_CONTAINER} with ID: ${EXISTING_DOCKER_CONTAINER_ID}."
+  docker stop ${EXISTING_DOCKER_CONTAINER_ID}
+  echo "Remove the container ${DOCKER_CONTAINER} with ID: ${EXISTING_DOCKER_CONTAINER_ID}."
+  docker rm ${EXISTING_DOCKER_CONTAINER_ID}
 fi
 
 ################################################################################
 
 # Build the Docker image.
-docker-compose -p ${PROJECT} -f ./docker/docker-compose.yml build
+docker-compose -p ${DOCKER_PROJECT} -f ./docker/docker-compose.yml build
--- docker/scripts/keep-container-running.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-################################################################################
-
-# Keep the Docker container running in the background.
-# https://stackoverflow.com/questions/30209776/docker-container-will-automatically-stop-after-docker-run-d
-tail -f /dev/null
