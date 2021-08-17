e87ea8dd04934225bc2a20a7e93560c821f2a020
Cristian Beltran <cristianbehe@gmail.com>
2021-08-17 17:17:38 +0900

    Add more info to logs

--- catkin_ws/src/o2ac_routines/src/o2ac_routines/base.py
+++ catkin_ws/src/o2ac_routines/src/o2ac_routines/base.py
@@ -1339,7 +1339,7 @@ class O2ACBase(object):
           rospy.logerr("Fail to complete playback sequence: %s" % sequence_name)
           return False
     else:
-      rospy.loginfo("(plan_while_moving) Sequence name: %s" % sequence_name)
+      rospy.loginfo("(plan_while_moving, %s) Sequence name: %s" % (robot_name, sequence_name))
       all_plans.append(robot_name)
       active_plan = None
       active_plan_start_time = rospy.Time(0)
@@ -1350,13 +1350,13 @@ class O2ACBase(object):
       for i, point in enumerate(sequence):
         gripper_action = None
 
-        rospy.loginfo("(plan_while_moving) Sequence point: %i - %s" % (i+1, point[0]))
+        rospy.loginfo("(plan_while_moving, %s) Sequence point: %i - %s" % (robot_name, i+1, point[0]))
         # self.confirm_to_proceed("playback_sequence")
 
         res = self.plan_waypoint(robot_name, point, previous_plan, end_effector_link=end_effector_link) # res = plan, planning_time 
 
         if not res:
-          rospy.logerr("Fail to complete playback sequence: %s" % sequence_name)
+          rospy.logerr("%s: Fail to complete playback sequence: %s" % (robot_name, sequence_name))
           return False
         
         if isinstance(res[0], dict):
@@ -1393,19 +1393,19 @@ class O2ACBase(object):
           remaining_time = execution_time - active_plan_duration
           if remaining_time < 0.1: # Not enough time for queue up another plan; wait for execution to complete
             if not robot.robot_group.wait_for_motion_result(45.0): # wait for motion max of 45 seconds
-              rospy.logerr("MoveIt aborted the motion")
-            rospy.loginfo("Waited for motion result")
+              rospy.logerr("%s: MoveIt aborted the motion" % robot_name)
+            rospy.loginfo("%s: Waited for motion result" % robot_name)
           else:
             # Try planning another point
             continue
           
           if not self.check_plan_goal_reached(robot_name, active_plan):
-            rospy.logerr("Failed to execute sequence plan: target pose not reached")
+            rospy.logerr("%s: Failed to execute sequence plan: target pose not reached" % robot_name)
             return False
 
         # execute next plan 
         next_plan, index, plan_type = backlog.pop(0)
-        print("Executing sequence plan: index,", index, "type", plan_type)
+        print(robot_name, ": Executing sequence plan: index,", index, "type", plan_type)
         wait = True if i == len(sequence) -1 else False
         self.execute_waypoint_plan(robot_name, next_plan, wait=wait)
 
@@ -1416,16 +1416,16 @@ class O2ACBase(object):
           active_plan_start_time = rospy.Time.now()
           active_plan_duration = helpers.get_trajectory_duration(next_plan)
 
+      # wait for last executed motion
       robot.robot_group.wait_for_motion_result(45.0) # wait for motion max of 45 seconds
       # Finished preplanning the whole sequence: Execute remaining waypoints
       while backlog:
-        robot.robot_group.wait_for_motion_result()
         if not self.check_plan_goal_reached(robot_name, active_plan):
-          rospy.logerr("Fail to execute plan: target pose not reach")
+          rospy.logerr("%s: Fail to execute plan: target pose not reach" % robot_name)
           return False
 
         next_plan, index, plan_type = backlog.pop(0)
-        print("Executing plan (backlog loop): index,", index, "type", plan_type)
+        print(robot_name, ": Executing plan (backlog loop): index,", index, "type", plan_type)
         self.execute_waypoint_plan(robot_name, next_plan, True)
         if isinstance(next_plan, (moveit_msgs.msg.RobotTrajectory)):
           active_plan = next_plan
